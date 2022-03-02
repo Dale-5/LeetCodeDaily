55fb0b7f330fc10df20236973486b34533216b38
dale <923946896@qq.com>
2022-03-02 14:14:52 +0800

    hot fix commit1

--- src/main/java/com/dale/leetcodedaily/day000000/Test.java
+++ src/main/java/com/dale/leetcodedaily/day000000/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("123test");
         System.out.println("third times");
+        System.out.println("hotfix edit");
     }
 }
